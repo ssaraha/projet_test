03a09ef71710b08316a5282a03e7784fb1afdc0f
aina <1ormartic@gmail.com>
2021-10-23 10:26:28 +0300

    Add account feature

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Form\UserFormType;
 
 class AccountController extends AbstractController
 {
@@ -13,8 +16,32 @@ class AccountController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('account/show.html.twig', [
-            'controller_name' => 'AccountController',
-        ]);
+        return $this->render('account/show.html.twig');
+    }
+
+    /**
+     * @Route("/account/edit", name="app_account_edit")
+     */
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+             $this->addFlash('success', 'Profile utilisateur modifié avec success.');
+
+            return $this->render('account/show.html.twig');
+            
+        }
+
+        return $this->render('account/edit.html.twig',
+                    [
+                        'form' => $form->createView()
+                    ] 
+                );
     }
 }
